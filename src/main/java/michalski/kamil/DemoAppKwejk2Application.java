4cc10c781248e4ecbeef1b38cb9740be031ea2a0
KamilMichalski <michalski.kamil@gmail.com>
2017-01-28 19:00:00 +0100

    first commit

---
+++ src/main/java/michalski/kamil/DemoAppKwejk2Application.java
@@ -0,0 +1,12 @@
+package michalski.kamil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppKwejk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppKwejk2Application.class, args);
+	}
+}
