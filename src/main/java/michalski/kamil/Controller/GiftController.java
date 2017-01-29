8219315c047f49faf8332b2ca6bd83c206c88674
KamilMichalski <michalski.kamil@gmail.com>
2017-01-29 11:30:24 +0100

    added gift  detalis

--- src/main/java/michalski/kamil/Controller/GiftController.java
+++ src/main/java/michalski/kamil/Controller/GiftController.java
@@ -1,6 +1,8 @@
 package michalski.kamil.Controller;
 
+import michalski.kamil.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +10,12 @@ public class GiftController {
     @GetMapping("/")
     public String listGifts(){
         return "home";
+    }
+
+    @GetMapping("/gif")
+    public String gitDetails(ModelMap modelMap){
+        Gif gif = new Gif("android-explosion","Kamil",true);
+        modelMap.put("gif",gif);
+        return "gif-details";
     }
 }
