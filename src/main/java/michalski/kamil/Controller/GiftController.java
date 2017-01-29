c22bca6b6bfaf0591a41dcaee9fae6a8c8379335
KamilMichalski <michalski.kamil@gmail.com>
2017-01-29 13:58:50 +0100

    added open onclick gifs

--- src/main/java/michalski/kamil/Controller/GiftController.java
+++ src/main/java/michalski/kamil/Controller/GiftController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GiftController {
@@ -15,21 +16,27 @@ public class GiftController {
 
 
     @GetMapping("/")
-    public String listGifts(ModelMap modelMap){
-        modelMap.addAttribute("gifs",gifRepository.getAllGifs());
+    public String listGifts(ModelMap modelMap) {
+        modelMap.addAttribute("gifs", gifRepository.getAllGifs());
         return "home";
     }
 
     @GetMapping("/gif")
-    public String gifDetails(ModelMap modelMap){
+    public String gifDetails(ModelMap modelMap) {
         Gif gif = gifRepository.findByName("android-explosion");
-        modelMap.put("gif",gif);
+        modelMap.put("gif", gif);
         return "gif-details";
     }
 
     @GetMapping("/favorites")
-    public String favorites(ModelMap modelMap){
-        modelMap.addAttribute("gifs",gifRepository.getFavorites());
+    public String favorites(ModelMap modelMap) {
+        modelMap.addAttribute("gifs", gifRepository.getFavorites());
         return "favorites";
+    }
+
+    @GetMapping("/gif/{name}")
+    public String gifDetalis(@PathVariable String name, ModelMap modelMap) {
+        modelMap.put("gif", gifRepository.findByName(name));
+        return "gif-details";
     }
 }
